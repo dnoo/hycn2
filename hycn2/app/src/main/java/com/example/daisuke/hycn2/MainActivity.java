73a4834000a5593a8b6f627b5f5bf98eab61b1c8
dnoo <ndoagiuscuhkie@gmail.com>
2018-07-12 04:12:23 +0900

    [fix]なんか動かないみたいだからぜんぶ上げるね

--- hycn2/app/src/main/java/com/example/daisuke/hycn2/MainActivity.java
+++ hycn2/app/src/main/java/com/example/daisuke/hycn2/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.daisuke.hycn2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoguchiClass nc; //noguchiClassのインスタンス
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nc = new NoguchiClass();//noguchiClassのインスタンス生成
+        tv = findViewById(R.id.text_view);
+        String str = nc.onTokenRefresh();
+        tv.setText(str);
     }
 }
