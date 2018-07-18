efed46292b5ec75c2df659e8347ac38f0fe9576b
Nanya Hayato <x4525062@edu.gifu-u.ac.jp>
2018-07-18 11:46:43 +0900

    Second commit

---
+++ hycn2/app/src/main/java/com/example/daisuke/hycn2/Main4Activity.java
@@ -0,0 +1,92 @@
+package com.example.daisuke.hycn2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main4Activity extends AppCompatActivity {
+
+    //定義
+    private EditText et01;
+    private EditText et02;
+    private EditText et03;
+    private EditText et04;
+    private EditText et05;
+    private EditText et06;
+    private EditText et07;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+    }
+
+
+
+
+
+    /*
+    main4et1 ~ main4et7
+    */
+    public void caction(View view) {//completeボタンの挙動
+
+        //紐付け
+        et01 =findViewById(R.id.main4et1);
+        et02 =findViewById(R.id.main4et2);
+        et03 =findViewById(R.id.main4et3);
+        et04 =findViewById(R.id.main4et4);
+        et05 =findViewById(R.id.main4et5);
+        et06 =findViewById(R.id.main4et6);
+        et07 =findViewById(R.id.main4et7);
+
+        String temp1 = et01.getText().toString();
+        String temp2 = et02.getText().toString();
+        String temp3 = et03.getText().toString();
+        String temp4 = et04.getText().toString();
+        String temp5 = et05.getText().toString();
+        String temp6 = et06.getText().toString();
+        String temp7 = et07.getText().toString();
+
+        if (temp1.equals("") == false && temp2.equals("") == false
+                && temp3.equals("") == false && temp4.equals("") == false
+                && temp5.equals("") == false && temp6.equals("") == false
+                && temp7.equals("") == false) {
+            //保存
+            saveFile("testfile.txt",temp1,temp2,temp3,temp4,temp5,temp6,temp7);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivityForResult(intent, 0);
+        }
+    }
+
+
+    // ファイルを保存
+    public void saveFile(String file, String str1, String str2, String str3
+            , String str4, String str5, String str6, String str7) {
+        try(FileOutputStream fileOutputstream = openFileOutput(file,
+                Context.MODE_PRIVATE);) {
+            fileOutputstream.write(str1.getBytes());
+            fileOutputstream.write(13);
+            fileOutputstream.write(str2.getBytes());
+            fileOutputstream.write(13);
+            fileOutputstream.write(str3.getBytes());
+            fileOutputstream.write(13);
+            fileOutputstream.write(str4.getBytes());
+            fileOutputstream.write(13);
+            fileOutputstream.write(str5.getBytes());
+            fileOutputstream.write(13);
+            fileOutputstream.write(str6.getBytes());
+            fileOutputstream.write(13);
+            fileOutputstream.write(str7.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- hycn2/app/src/main/java/com/example/daisuke/hycn2/MainActivity.java
+++ hycn2/app/src/main/java/com/example/daisuke/hycn2/MainActivity.java
@@ -1,28 +1,92 @@
 package com.example.daisuke.hycn2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
-
-
-
-
-
+// maintv1 ~ maintv7
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //定義
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView tv4;
+    private TextView tv5;
+    private TextView tv6;
+    private TextView tv7;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //紐付け
+        tv1 = findViewById(R.id.maintv1);
+        tv2 = findViewById(R.id.maintv2);
+        tv3 = findViewById(R.id.maintv3);
+        tv4 = findViewById(R.id.maintv4);
+        tv5 = findViewById(R.id.maintv5);
+        tv6 = findViewById(R.id.maintv6);
+        tv7 = findViewById(R.id.maintv7);
+
+
+        //ファイル初期読み込み
+        String[] temp = readFile("testfile.txt");
+        if(temp != null) {
+            tv1.setText(temp[0]);
+            tv2.setText(temp[1]);
+            tv3.setText(temp[2]);
+            tv4.setText(temp[3]);
+            tv5.setText(temp[4]);
+            tv6.setText(temp[5]);
+            tv7.setText(temp[6]);
+        }
+        //ファイル初期読み込み
+
+
+        /*変更前3
+        //アクション登録
+        Bt01.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //保存箇所
+                String temp = et01.getText().toString();
+                saveFile("testfile.txt",temp);
+
+                //読み込み箇所
+                String temp2 = readFile("testfile.txt");
+                if(temp2 != null) {
+                    ev01.setText(temp2);
+                } else {
+                    ev01.setText("error");
+                }
+
+            }
+        });
+        */
     }
 
+    //画面4への遷移
     public void changescreen01(View view) {
         //TextView tv = findViewById(R.id.mytext1);
         //tv.setText("Changed!");
@@ -30,30 +94,171 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 0);
     }
 
+    //画面3への遷移
     public void changescreen02(View view) {
         //TextView tv = findViewById(R.id.mytext1);
         //tv.setText("Changed!");
         Intent intent = new Intent(this, Main3Activity.class);
         startActivityForResult(intent, 0);
     }
+
+    //個人情報登録画面へ遷移
+    public void rnclick(View view) {
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivityForResult(intent, 0);
+    }
+
+
+
+    // ファイルの保存
+    public void saveFile(String file, String str) {
+        try(FileOutputStream fileOutputstream = openFileOutput(file,
+                Context.MODE_PRIVATE);) {
+
+            fileOutputstream.write(str.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    // ファイルを読み出し
+    public String[] readFile(String file){
+
+        String[] text = {null, null, null, null, null, null, null};
+
+        try(FileInputStream fileInputStream = openFileInput(file);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(fileInputStream,"UTF-8"));
+            ) {
+
+            int i = 0;
+            String lineBuffer;
+            while ((lineBuffer = reader.readLine()) != null) {
+                text[i] = lineBuffer;
+                Log.d(text[i], "readFile: ");
+                Log.d(""+i+"", "number: ");
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
+
+
+
 }
 
 
 
 
-////////original codes////////
 /*
-public class MainActivity extends AppCompatActivity {
+///////////////
+/////////
+////ここから下はメモ
+//////////////
+////////
+////
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    1
+    //ファイルの保存(original)
+    public void saveFile(String file, String str) {
+        try(FileOutputStream fileOutputstream = openFileOutput(file,
+                Context.MODE_PRIVATE);) {
+
+            fileOutputstream.write(str.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void changelabel(View view) {
-        TextView tv = findViewById(R.id.mytext1);
-        tv.setText("Changed!");
+
+    2
+    // ファイルを読み出し(original)
+    public String readFile(String file){
+        String text = null;
+
+        try(FileInputStream fileInputStream = openFileInput(file);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(fileInputStream,"UTF-8"));
+            ) {
+
+            String lineBuffer;
+            while ((lineBuffer = reader.readLine()) != null) {
+                text = lineBuffer;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
-}*/
-///////////////////////////////
+
+
+    3
+    //OnClick登録
+    public void testclick(View view) {
+        String temp = et01.getText().toString();
+        ev01.setText(temp);
+    }
+
+
+    4
+    //定義(class MainActivityとonCreateの間)
+    private EditText et01;
+    private TextView ev01;
+
+
+    5
+    //紐付け(onCreateの直後)
+    et01 =findViewById(R.id.my02);
+    ev01 = findViewById(R.id.my01);
+    Button Bt01 = findViewById(R.id.my03);
+
+
+    6
+    //ファイル初期読み込み1(onCreate内)
+    String temp3 = readFile("testfile.txt");
+    if(temp3 != null) {
+        ev01.setText(temp3);
+    }
+    //ファイル読み込み1
+
+
+    7
+    //アクション登録
+    Bt01.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            //保存箇所
+            String temp = et01.getText().toString();
+            saveFile("testfile.txt",temp);
+
+            //読み込み箇所
+            String temp2 = readFile("testfile.txt");
+            if(temp2 != null) {
+                ev01.setText(temp2);
+            } else {
+                ev01.setText("error");
+            }
+
+        }
+    });
+
+
+
+
+///////////////
+/////////
+////ここから上はメモ
+//////////////
+////////
+////
+*/
+
+
+
+
