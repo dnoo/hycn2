daef9685181e151e57d59a079c49d45c1bd07350
7t1y1r0 <tayori7171@gmail.com>
2018-07-20 02:31:49 +0900

    個人画面のデータベースからの読み取りと表示まで．

--- hycn2/app/src/main/java/com/example/daisuke/hycn2/Main5Activity.java
+++ hycn2/app/src/main/java/com/example/daisuke/hycn2/Main5Activity.java
@@ -8,11 +8,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Map;
 
 // main5tv1 ~ main5tv7
 
@@ -39,12 +44,36 @@ public class Main5Activity extends AppCompatActivity {
     private TextView tv24;
     private TextView tv25;
 
+    //グローバル変数
+    globalsClass globals;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //userProfile読み込みここから
+        Log.d("userProfile読み込み","読み込み開始");
+        //グローバル変数を取得
+        globals = (globalsClass) this.getApplication();
+        if(!globals.mapMyProfile.isEmpty()){
+            Log.d("myProfileMap",globals.mapMyProfile.toString());
+            //mapの確認
+            for (Map.Entry<String , String> myProfileMap : globals.mapMyProfile.entrySet()) {
+                Log.d("myProfileMap",myProfileMap.getKey()+":"+myProfileMap.getValue());
+            }
+        }else {
+            Log.d("myProfileMap", "Error matMyProfile is Empty");
+            // Go to MainActivity
+            startActivity(new Intent(Main5Activity.this, Main5Activity.class));
+            finish();
+            return ;
+        }
         setContentView(R.layout.activity_main5);
+        //userProfile読み込みここまで
 
         //紐付け
+        //基本情報
         tv1 = findViewById(R.id.main5tv1);
         tv2 = findViewById(R.id.main5tv2);
         tv3 = findViewById(R.id.main5tv3);
@@ -53,23 +82,77 @@ public class Main5Activity extends AppCompatActivity {
         tv6 = findViewById(R.id.main5tv6);
         tv7 = findViewById(R.id.main5tv7);
 
+        //趣味のジャンル
         tv11 = findViewById(R.id.textView521);
         tv12 = findViewById(R.id.textView522);
         tv13 = findViewById(R.id.textView523);
         tv14 = findViewById(R.id.textView524);
         tv15 = findViewById(R.id.textView525);
-
+        //趣味のコメント
         tv21 = findViewById(R.id.main52tv1);
         tv22 = findViewById(R.id.main52tv2);
         tv23 = findViewById(R.id.main52tv3);
         tv24 = findViewById(R.id.main52tv4);
         tv25 = findViewById(R.id.main52tv5);
 
+        tv1.setText(globals.mapMyProfile.get("name"));
+        tv2.setText(globals.mapMyProfile.get("number"));
+        tv3.setText(globals.mapMyProfile.get("faculty"));
+        tv4.setText(globals.mapMyProfile.get("circle"));
+        tv5.setText(globals.mapMyProfile.get("birth"));
+        tv6.setText(globals.mapMyProfile.get("age"));
+        tv7.setText(globals.mapMyProfile.get("comment"));
+
+        //趣味取り出し
+        try{
+            JSONObject JSONHobby = new JSONObject(globals.mapMyProfile.get("hobby").toString());
+            Log.d("JSONHobby",JSONHobby.toString(2));
+            Iterator<String> keys = JSONHobby.keys();
+            int count = 0;
+            while(keys.hasNext()){
+                String key = keys.next();
+                String value = JSONHobby.getString(key);
+                Log.d("Main5Activity", "キー名 = " + key + "   :   値 = " + value);
+                switch(count){
+                    case 0:
+                        tv11.setText(key);
+                        tv21.setText(value);
+                        break;
+                    case 1:
+                        tv12.setText(key);
+                        tv22.setText(value);
+                        break;
+                    case 2:
+                        tv13.setText(key);
+                        tv23.setText(value);
+                        break;
+                    case 3:
+                        tv14.setText(key);
+                        tv24.setText(value);
+                        break;
+                    case 4:
+                        tv15.setText(key);
+                        tv25.setText(value);
+                        break;
+                    default :
+                        break;
+                }
+                count++;
+            }
+        } catch (JSONException e){
+
+        }
+
+
+
+
+/*
         //ファイル初期読み込み
         String[] temp = readFile("testfile.txt");
         String[] temp2 = readFile2("testfile2.txt");//スピナー情報
         String[] temp3 = readFile2("testfile3.txt");//コメント情報
         if(temp != null) {
+            Log.d("tv1",temp[0]);
             tv1.setText(temp[0]);
             tv2.setText(temp[1]);
             tv3.setText(temp[2]);
@@ -80,6 +163,7 @@ public class Main5Activity extends AppCompatActivity {
         }
 
         if(temp2 != null) {
+            Log.d("tv11",temp2[0]);
             tv11.setText(temp2[0]);
             tv12.setText(temp2[1]);
             tv13.setText(temp2[2]);
@@ -88,6 +172,7 @@ public class Main5Activity extends AppCompatActivity {
         }
 
         if(temp3 != null) {
+            Log.d("tv21",temp3[0]);
             tv21.setText(temp3[0]);
             tv22.setText(temp3[1]);
             tv23.setText(temp3[2]);
@@ -95,10 +180,16 @@ public class Main5Activity extends AppCompatActivity {
             tv25.setText(temp3[4]);
         }
         //ファイル初期読み込み
+*/
+
     }
 
 
+    @Override
+    public void onStart(){
+        super.onStart();
 
+    }
 
 
 
